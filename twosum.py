dc0c42490fb5ea1188293e2e3737dc8f56c8e8d8
Riley Harper <riley.harper@unc.edu>
2024-07-02 11:42:27 -0400

    Update twosum.py

--- twosum.py
+++ twosum.py
@@ -1,3 +1,7 @@
+### Two Sum - Completed July 2nd, 2024
+## Runtime of 3801 ms beats 5%
+## Memory of 17.29 MB beats 91.29%
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         correct = []
